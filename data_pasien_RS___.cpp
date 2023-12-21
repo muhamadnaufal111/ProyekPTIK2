9ec066f36992a218ca5042725071e8b6deb8bfd0
muhamadnaufal111 <opangidm083@gmail.com>
2023-12-21 21:05:07 +0700

    commit

---
+++ data_pasien_RS___.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib> // for system("cls")
+#include <conio.h>
+
+using namespace std;
+
+struct Data {
+    char no_rekam_medis[20], nama_pasien[20], tanggal_lahir[20], usia[20], jenis_kelamin[20], no_telepon[20], berat_badan[20], tinggi_badan[20], keluhan_utama[20], tanggal_masuk[20], alamat[20];
+};
