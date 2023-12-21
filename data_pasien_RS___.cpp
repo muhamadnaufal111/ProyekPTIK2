154e525732b96b375343dc3452d950e53b30b627
CindiNuraeni18 <cindinuraeni@gmail.com>
2023-12-21 22:10:27 +0700

    tampildata

--- data_pasien_RS___.cpp
+++ data_pasien_RS___.cpp
@@ -46,3 +46,22 @@ void inputData() {
         ++jumlahData;
     }
 }
+
+void tampilkanData() {
+    system("cls");
+    cout << "\n========================================================================= Menampilkan Data Pasien =================================================================================\n\n";
+    cout << left << setw(5) << "No" << setw(18) << "No Rekam Medis" << setw(20) << "Nama Pasien" << setw(20) << "Tanggal Lahir"
+         << setw(8) << "Usia" << setw(15) << "Jenis Kelamin" << setw(15) << "No Telepon" << setw(15) << "Berat Badan"
+         << setw(15) << "Tinggi Badan" << setw(20) << "Keluhan Utama" << setw(15) << "Tanggal Masuk" << setw(20) << "Alamat" << endl;
+
+    for (int i = 0; i < jumlahData; ++i) {
+        cout << left << setw(5) << i + 1 << setw(18) << pasien[i].no_rekam_medis << setw(20) << pasien[i].nama_pasien
+             << setw(20) << pasien[i].tanggal_lahir << setw(8) << pasien[i].usia << setw(15) << pasien[i].jenis_kelamin
+             << setw(15) << pasien[i].no_telepon << setw(15) << pasien[i].berat_badan << setw(15) << pasien[i].tinggi_badan
+             << setw(20) << pasien[i].keluhan_utama << setw(15) << pasien[i].tanggal_masuk << setw(20) << pasien[i].alamat << endl;
+    }
+
+    cout << "\n===================================================================================================================================================================================\n";
+    getch(); // Menunggu sampai tombol ditekan
+    system("cls");
+}
